3fde4faecf904a90fb764565748d4655e7cd1ea8
Helder <ssavess@gmail.com>
2017-11-08 19:59:39 +0000

    1
    1

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
-path = __dirname + "/"+ "public";
+//var path = __dirname + "/public/views/";
+//path = __dirname + "/public/views/";
 //var port = process.env.PORT;
-var port = 3000;
+var port = process.env.PORT || 3000;
 
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html');
+app.set('views', path.resolve(__dirname,'public','views'));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/public/views/index.html');
   })
   
-
-
-
 app.listen(port, function () {
     console.log('dirname...: ' + __dirname);
     console.log('path...: ' + path);
